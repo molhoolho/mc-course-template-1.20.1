f6cc97ce19cc2cd0cc298086a181bda5ee9bfc8e
FelipeF <felipefdspina@gmail.com>
2024-06-27 19:30:51 -0300

    Initial commit

---
+++ src/main/java/net/molho/mccourse/MCCourseDataGenerator.java
@@ -0,0 +1,11 @@
+package net.molho.mccourse;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MCCourseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/molho/mccourse/MCCourseModClient.java
@@ -0,0 +1,11 @@
+package net.molho.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
